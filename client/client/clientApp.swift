5b36f4e9bcaf61ee86fbe1b204db5ebc0d7a4763
j-rockwell <john@llewkcor.com>
2025-07-13 23:49:29 -0700

    init

---
+++ client/client/clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  clientApp.swift
+//  client
+//
+//  Created by John Rockwell on 7/13/25.
+//
+
+import SwiftUI
+
+@main
+struct clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ client/clientTests/clientTests.swift
@@ -0,0 +1,17 @@
+//
+//  clientTests.swift
+//  clientTests
+//
+//  Created by John Rockwell on 7/13/25.
+//
+
+import Testing
+@testable import client
+
+struct clientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
